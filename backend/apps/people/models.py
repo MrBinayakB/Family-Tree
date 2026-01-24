7f55c0a429cc884fcc7b9a711fcfab11239881ce
MrBinayakB <b.binayak07@gmail.com>
2026-01-24 22:04:30 +0545

    Tree Model is created and People model updated

--- backend/apps/people/models.py
+++ backend/apps/people/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
-# Create your models here.
-class PeopleModel(models.Model):
+from trees.models import Trees
+class People(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     firstname = models.CharField(max_length=100, null=False)
     familyname = models.CharField(max_length=100, null=False)
     birth_date = models.DateField()
     gender = models.CharField(max_length=10)
+    tree = models.ForeignKey(Trees, on_delete=models.CASCADE)
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class UserModel(models.Model):
+class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(null=False)
     password = models.CharField(max_length=32, null=False)
