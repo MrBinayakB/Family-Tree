23ec2dc9dc721184863c9c0b632fc294e79b89d7
MrBinayakB <b.binayak07@gmail.com>
2026-01-24 22:37:15 +0545

    Relation Model minor update

--- backend/apps/relationship/models.py
+++ backend/apps/relationship/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-class relation(models.Model):
+class Relation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
+    metadata_json = models.JSONField(null=True, blank=True)
