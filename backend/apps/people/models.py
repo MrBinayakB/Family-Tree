a387f3548fb85bebe904a9af902d99e8aae65101
MrBinayakB <b.binayak07@gmail.com>
2026-01-24 21:43:15 +0545

    DB model of People and Users is created

--- backend/apps/people/models.py
+++ backend/apps/people/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class PeopleModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    firstname = models.CharField(max_length=100, null=False)
+    familyname = models.CharField(max_length=100, null=False)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=10)
